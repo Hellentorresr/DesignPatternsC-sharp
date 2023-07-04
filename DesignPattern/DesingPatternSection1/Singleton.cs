9aba8452fcd564180593c427b09418a1ddab65d2
Hellentorresr <htorres1@ucenfotec.ac.cr>
2023-07-04 12:07:53 -0600

    Learning about Singleton design pattern

---
+++ DesignPattern/DesingPatternSection1/Singleton.cs
@@ -0,0 +1,26 @@
+﻿namespace DesingPatternSection1
+{
+    public class Singleton
+    {
+        static Singleton? instance; //Self referencing property
+        public string Setting { get; set; } = "color blue";
+
+        //the constructor is protected
+        protected Singleton() { }
+
+        //creating a static method that is
+        public static Singleton Instance()
+        {
+            instance ??= new Singleton();// is using the null-coalescing assignment operator (??=) to assign a new instance of the Singleton class to the variable instance if it is currently null.
+            return instance;
+        }
+    }
+}
+
+
+/*Typically, in the singleton pattern, you would have a private constructor
+ * in the Singleton class, preventing the direct creation of instances from outside the class.
+ * Instead, you would typically use a static method or property
+ * to access the single instance, like Singleton.Instance or Singleton.GetInstance().
+ * Provide a static property or method in the Singleton class that returns the single instance of the class.
+ */
---
+++ DesignPattern/Tester/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DesingPatternSection1;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Singleton object1 = new();//this is not allow due to the its protection lavel
+
+        Singleton Object1 = Singleton.Instance();//instance is the public static method
+        Singleton Object2 = Singleton.Instance();
+
+        Console.WriteLine(Object1.Setting);//color blue
+        Console.WriteLine(Object2.Setting);//color blue
+
+        //let's ckeck without a doubt that this is a singleton
+        //this is going to check in the momory to make sure that it's the same exact object
+        //if it's the same place in memory it's going to console log this message
+        if (Object1 == Object2)
+        {
+            Console.WriteLine("This objects are the same ");
+        }
+    }
+}
